19bc24d4f161589efa952722d968c9cfcc9271f3
STARoM00N <tarte.2548@gmail.com>
2024-11-21 01:57:48 +0700

    test2.8.1
    connect no error

--- config/Database.php
+++ config/Database.php
@@ -1,14 +1,31 @@
 <?php
 class Database {
-    private $host = "tcp:prohectcs436database.database.windows.net,1433"; // Server name
-    private $db = "ProjectCS436"; // Database name
-    private $username = "projectcs436"; // Username
-    private $password = ".cs436team"; // Replace with your actual password
+    private $host = "tcp:prohtectcs436database.database.windows.net,1433"; // ชื่อเซิร์ฟเวอร์
+    private $db = "ProjectCS436"; // ชื่อฐานข้อมูล
+    private $username = "projectcs436"; // ชื่อผู้ใช้
+    private $password = ".cs436team"; // รหัสผ่าน
     public $conn;
 
+    class Database{
+        private $host = "localhost";
+        private $db = "login_regis_db";
+        private $username = "root";
+        private $password = "";
+        public $conn;
     public function getConnection() {
         $this->conn = null;
 
+        public function getConnection(){
+            $this -> conn = null;
+
+            try{
+                $this->conn = new PDO("mysql:host=".$this->host. ";dbname=". $this->db, $this->username, $this->password);
+            }
+            catch(PDOException $exception){
+                echo "Connection Error: ". $exception->getMessage();
+            }
+
+            return $this->conn;
         try {
             $this->conn = new PDO(
                 "sqlsrv:server=$this->host;Database=$this->db",
@@ -16,11 +33,12 @@ class Database {
                 $this->password
             );
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connection successful!";
         } catch (PDOException $e) {
             echo "Connection Error: " . $e->getMessage();
         }
+    }
 
+?>
         return $this->conn;
     }
 }
