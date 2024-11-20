fd704030d50fb8024a4c7775be0a8b28732f6417
STARoM00N <tarte.2548@gmail.com>
2024-11-21 05:02:56 +0700

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -1,15 +1,18 @@
 <?php
+
 class Database {
-    private $host = "tcp:prohectcs436database.database.windows.net,1433"; // Azure SQL Server Host
-    private $db = "ProjectCS436"; // Database Name
+    private $host = "tcp:projectcs436database.database.windows.net,1433"; // Azure SQL Server Host
+    private $db_name = "ProjectCS436"; // Database Name
     private $username = "projectcs436"; // Database Username
-    private $password = ".cs436team"; // Database Password (แทนที่ด้วยรหัสผ่านจริงของคุณ)
+    private $password = "cs436team@"; // Database Password (แทนที่ด้วยรหัสผ่านจริง)
+
     public $conn;
 
     public function getConnection() {
         $this->conn = null;
 
         try {
+            // ตรวจสอบการใช้งานตัวแปรให้ถูกต้อง
             $dsn = "sqlsrv:server=$this->host;Database=$this->db_name";
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -21,5 +24,4 @@ class Database {
         return $this->conn;
     }
 }
-
 ?>
