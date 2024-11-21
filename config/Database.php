0f2bab6f8c30af69d1123729ed0fccf2e948ec0a
STARoM00N <tarte.2548@gmail.com>
2024-11-21 18:52:24 +0700

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -1,6 +1,6 @@
 <?php
 class Database {
-    private $host = "tcp:prohectcs436database.database.windows.net,1433"; // เปลี่ยนเป็น Host ของ Azure
+    private $host = "prohectcs436database.database.windows.net"; // Host ของ SQL Server
     private $db_name = "ProjectCS436"; // ชื่อฐานข้อมูล
     private $username = "projectcs436"; // Username
     private $password = ".cs436team"; // Password
@@ -10,8 +10,12 @@ class Database {
         $this->conn = null;
 
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->exec("set names utf8");
+            $this->conn = new PDO(
+                "sqlsrv:Server=" . $this->host . ";Database=" . $this->db_name, 
+                $this->username, 
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // ตั้งค่า Error Mode
         } catch (PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
