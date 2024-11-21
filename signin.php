5592529310d9fc2cb385e6dd8349e97709d37234
STARoM00N <tarte.2548@gmail.com>
2024-11-21 17:06:22 +0700

    Update signin.php

--- signin.php
+++ signin.php
@@ -29,3 +29,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ob_end_flush();
 ?>
+
+<div class="container">
+    <h3 class="my-3">Login Page</h3>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+        <div class="mb-3">
+            <label for="username" class="form-label">Username</label>
+            <input type="text" name="username" id="username" class="form-control" placeholder="Enter your username" required>
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" name="password" id="password" class="form-control" placeholder="Enter your password" required>
+        </div>
+        <div class="mb-3">
+            <a href="index.php" class="btn btn-secondary">Go Back</a>
+            <button type="submit" class="btn btn-primary">Sign In</button>
+        </div>
+        <p class="mt-3">No account? <a href="signup.php">Create one!</a></p>
+    </form>
+</div>
